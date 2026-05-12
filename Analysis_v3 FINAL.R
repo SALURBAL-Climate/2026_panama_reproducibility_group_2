ce328b4300254b11fa1b13c49d8647a2fa6ac559
Diana Higuera <drh89@drexel.edu>
2026-05-12 15:40:41 -0400

    Separate exploratory analysis

--- Analysis_v3 FINAL.R
+++ Analysis_v3 FINAL.R
@@ -7,16 +7,6 @@ library(SALURhelper)
 # READ FILES
 
 
-# Exploratory plots and tables
-plot(df$date, df$deaths)
-plot(df$date, df$temp)
-plot(df$date, df$pm25)
-plot(df$temp, df$pm25)
-plot(df$deaths, df$temp)
-plot(df$deaths, df$pm25)
-
-df |> 
-  summarize(deaths = sum(deaths), temp = mean(temp))
 
 # Model 1
 cbt1 <- crossbasis(
