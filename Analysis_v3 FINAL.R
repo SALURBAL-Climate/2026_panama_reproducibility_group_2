b588aa19d331720de6bf5bd11d6e9cf8a78cb258
Diana Higuera <drh89@drexel.edu>
2026-05-12 15:40:14 -0400

    Create a separate file for data wrangilng

--- Analysis_v3 FINAL.R
+++ Analysis_v3 FINAL.R
@@ -6,15 +6,6 @@ library(SALURhelper)
 
 # READ FILES
 
-df <- df |> 
-  group_by(across(c(sim_id, date))) |> 
-  summarize(
-    deaths = sum(deaths), cardio = sum(cardio), respdisease = sum(respdisease), respinf = sum(respinf),
-    temp = mean(temp), pm25 = mean(pm25)
-  ) |> 
-  ungroup() |> 
-  mutate(strata = paste0(year(date), ":", month(date), ":", wday(date))) |> 
-  mutate(strata = as_factor(strata))
 
 # Exploratory plots and tables
 plot(df$date, df$deaths)
